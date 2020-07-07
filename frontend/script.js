e24f92690be2c9ee7c58fae9f9e43c85d73b1b8a
limlance99 <limlance_99@yahoo.com>
2020-07-07 11:13:58 +0800

    added basic script

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,16 @@
 import {rail_paths} from "./constants/rail_paths";
 
-rails = document.getElementsByClassName('rails');
+rails = document.getElementsByClassName('rail');
 
-function send_paths() {
-    var edges = [];
-    for (let rail of rails) {
-        if (rail.style.backgroundColor != "#FFFFFF") edges.push(rail_paths[rail.id]);
-    }
+// function send_paths() {
+//     var edges = [];
+//     for (let rail of rails) {
+//         if (rail.style.backgroundColor != "#FFFFFF") edges.push(rail_paths[rail.id]);
+//     }
 
-    console.log(edges);
+//     console.log(edges);
+// }
+
+function rail_click(id, user) {
+    
 }
